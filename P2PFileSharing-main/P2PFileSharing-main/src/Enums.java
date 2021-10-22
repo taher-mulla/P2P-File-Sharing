3eb0f817195811148aa6cec0784921aeea3a7436
taher-mulla <68025686+taher-mulla@users.noreply.github.com>
2021-10-22 19:00:16 -0400

    Add files via upload

---
+++ P2PFileSharing-main/P2PFileSharing-main/src/Enums.java
@@ -0,0 +1,13 @@
+public class Enums {
+    public enum MessageType {
+        CHOKE,
+        UNCHOKE,
+        INTERESTED,
+        NOTINTERESTED,
+        HAVE,
+        BITFIELD,
+        REQUEST,
+        PIECE,
+        HANDSHAKE
+    }
+}
